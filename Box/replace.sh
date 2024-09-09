2d1d62066609b15994a6cc1e02556fae25cd4a49
RexYu <yuwenzhen@skg.com>
2024-09-09 20:44:55 +0800

    编译替换

--- Box/replace.sh
+++ Box/replace.sh
@@ -1,11 +1,14 @@
 #/bin/sh
 
-path=`pwd`
-workspace=$path
+path=$(pwd)
+workspace="$path/TVBoxOSC"
 echo $workspace
 cd $workspace
 
+cp -rf ../Box/* ./
+
+
 ## 替换APP名字
-sed -i 's/rootProject.name = "超级TV"/rootProject.name = "TVBOX"/g' ./settings.gradle
-sed -i 's/<string name="app_name">Tvbox<\/string>/<string name="app_name">超级TV<\/string>/g' ./app/src/main/res/values-zh/strings.xml
+sed -i 's/rootProject.name = "TVBox"/rootProject.name = "超级TV"/g' ./settings.gradle
+sed -i 's/<string name="app_name">TVBox<\/string>/<string name="app_name">超级TV<\/string>/g' ./app/src/main/res/values-zh/strings.xml
 
