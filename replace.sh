e6df25c474c8893b64db41ef07553cc3f7f02f33
RexYu <yuwenzhen@qq.com>
2024-09-09 21:01:14 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -2,13 +2,12 @@
 
 path=$(pwd)
 workspace="$path/TVBoxOSC"
-echo $workspace
-cd $workspace
+echo $path
+#cd $workspace
 
 cp -rf ../Box/* ./
-
-
 ## 替换APP名字
 sed -i 's/rootProject.name = "TVBox"/rootProject.name = "超级TV"/g' ./settings.gradle
 sed -i 's/<string name="app_name">TVBox<\/string>/<string name="app_name">超级TV<\/string>/g' ./app/src/main/res/values-zh/strings.xml
 
+cat ./settings.gradle
